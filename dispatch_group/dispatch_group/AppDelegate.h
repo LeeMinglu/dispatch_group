32e7ef4a10c549558fdc4f7d6a9f3cdcbd4b37e9
LeeMinglu <402007353@qq.com>
2016-01-02 21:18:52 +0800

    初始化项目

---
+++ dispatch_group/dispatch_group/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dispatch_group
+//
+//  Created by 李明禄 on 16/1/2.
+//  Copyright © 2016年 SocererGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dispatch_group/dispatch_group/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dispatch_group
+//
+//  Created by 李明禄 on 16/1/2.
+//  Copyright © 2016年 SocererGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
